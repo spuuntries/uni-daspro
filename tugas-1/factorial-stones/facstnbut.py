ef069d8793559de12ab368f2fc4a95f4135d63d0
spuuntries <kek@spuun.art>
2023-09-20 10:13:14 +0700

    fix: Add docs to facstnbut

--- tugas-1/factorial-stones/facstnbut.py
+++ tugas-1/factorial-stones/facstnbut.py
@@ -6,6 +6,9 @@ i = 1
 
 while True:
     r = inp // (5**i)
+    # input dibagi 5^i sampe 1<
+    # jumlah nol dibelakang angkanya adalah semua hasil dijumlahkan
+    # https://en.wikipedia.org/wiki/Trailing_zero
     if r < 1:
         break
     sum += r
