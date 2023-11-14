211c19a4de3ce08775ba1028fb215e81e0c93cb8
spuuntries <kek@spuun.art>
2023-11-14 22:17:37 +0700

    feat: Add `ibu`

---
+++ tugas-5/ibu/ibu.py
@@ -0,0 +1,82 @@
+m, n = map(int, input().split())
+maze = [list(map(int, input().split())) for _ in range(n)]
+
+
+def reconstruct(start, target, parents, path=None):
+    if path is None:
+        path = []
+
+    if target != start:
+        path.insert(0, target)
+        target = parents[target]
+        return reconstruct(start, target, parents, path)
+
+    path.insert(0, start)
+    return path
+
+
+def solve(maze, current=(0, 0), last_branch=None, parents=None, visited=None):
+    if not maze[0][0]:
+        return None
+
+    if visited is None:
+        visited = [(0, 0)]
+
+    if last_branch is None:
+        last_branch = []
+
+    if parents is None:
+        parents = {}
+
+    if current[0] == len(maze) - 1 and current[1] == len(maze[0]) - 1:
+        return reconstruct((0, 0), (len(maze) - 1, len(maze[0]) - 1), parents)
+
+    if len(maze) == 1 and len(maze[0]) == 1:
+        return visited
+
+    neighbors = list(
+        map(
+            lambda y: y[0],
+            filter(
+                lambda x: bool(x[1]) and x[0] not in visited,
+                dict.items(
+                    {
+                        (current[0] + 1, current[1]): maze[current[0] + 1][current[1]]
+                        if current[0] != len(maze) - 1
+                        else None,  # Checks down
+                        (current[0], current[1] + 1): maze[current[0]][current[1] + 1]
+                        if current[1] != len(maze[0]) - 1
+                        else None,  # Checks right
+                    }
+                ),
+            ),
+        )
+    )
+
+    if current not in visited:
+        visited += [current]
+
+    if not neighbors:
+        if last_branch:
+            last = last_branch.pop()
+            return solve(maze, last, last_branch, parents, visited)
+        else:
+            return None
+
+    if len(neighbors) == 2:
+        last_branch += [current]
+
+    for n in neighbors:
+        parents[n] = current
+
+    return solve(maze, neighbors[-1], last_branch, parents, visited)
+
+
+grid = [[0] * m for _ in range(n)]
+res = solve(maze)
+if not res:
+    print("Hari ini Bunda nggak masak")
+    exit(1)
+for i in res:
+    grid[i[0]][i[1]] = 1
+print("\n".join([" ".join(map(str, x)) for x in grid]))
